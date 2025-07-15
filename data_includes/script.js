e3943c9e2d7911750da5c25663c3a71d0eacb882
Dri0608 <drcaryca@gmail.com>
2025-07-15 18:49:46 -0300

    Update script.js

--- data_includes/script.js
+++ data_includes/script.js
@@ -39,14 +39,14 @@ newTrial("Participante",
 
 .log("NOME", getvar("NOME"))
 
-Template("tabela",  row=>
+Template("tabela.csv",  row=>
                       newTrial("Experimento",
 
                       newAudio("Audio", row.Audio)
-                         play()
+                         .play()
                       ,
 
-                      newVideo("Video")
+                      newVideo("Video", row.Video)
                          .size(90,90)
                          .play()
                          .print()
@@ -66,14 +66,19 @@ Template("tabela",  row=>
                          .add("center at 25%", "middle at 2%", getText("julgamentoN"))
                          .add("center at 75%, "middle at2%", getText("julgamentoE")
                          .print()
+                        ,
 
                      newselector()
                         .add(getText("julgamentoN"), getText("julgamentoE")
                         .keys("julgamentoN", "julgamentoE")
                         .log()
                         .wait()
-
 )
+.log("Áudio", row.Audio)
+.log("Vídeo", row.Video)
+.log("NOME", getVar("NOME"))
+
+);
 
 newtrial("Final",
 
