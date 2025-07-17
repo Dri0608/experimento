4110e5a183f4217b6b873f4fa61bd80ca7ffaa25
Dri0608 <drcaryca@gmail.com>
2025-07-17 09:57:48 -0300

    Update script.js

--- data_includes/script.js
+++ data_includes/script.js
@@ -1,91 +1,81 @@
 PennController.ResetPrefix(null);
-PennController.DebugOff
+PennController.DebugOff();
 
 Sequence("Participante", "Experimento", "Final");
 
 Header(
-
   defaultText
-    .css("font-size","1.2en")
+    .css("font-size","1.2em")
     .print()
   ,
-
-defaultTextInput
-    .css("font-size","1.2en")
+  defaultTextInput
+    .css("font-size","1.2em")
     .print()
   ,
-
-defaultButton
-    .css("font-size","1.2en")
+  defaultButton
+    .css("font-size","1.2em")
     .print()
     .center()
     .wait()
-
-)
-
-newTrial("Participante", 
-
-        newText("<p>Bem-vindo!</p>")
-        ,
-        newText("<p>Informe o seu nome:<p>")
-        ,
-        newTextInput("Nome")
-        ,
-        newButton("Iniciar")
-        ,
-        newVar("NOME")
-        .global()
-        .set(getTextInput("Nome"))
-)
-
-.log("NOME", getVar("NOME"))
-
-Template("tabela.csv",  row=>
-                      newTrial("Experimento",
-                               
-                      newVideo("Video", row.Video)
-                         .size(240,240)
-                         .center()
-                         .print()
-                         .play()
-                         .wait()
-                      ,
-
-                      newText("A", row.julgamentoN)
-                      ,
-                      newText("B", row.julgamentoE)
-                      ,
-
-                      newCanvas("2000wn", "800vh")
-                         .add("center at 25%", "middle at 2%", getText("julgamentoN"))
-                         .add("center at 75%", "middle at2%", getText("julgamentoE"))
-                         .print()
-                        ,
-
-                     newSelector()
-                        .add(getText("julgamentoN"), getText("julgamentoE"))
-                        .keys("julgamentoN", "julgamentoE")
-                        .log()
-                        .wait()
-                        ,
-
-                     newButton("Próximo")
-                      .log()
-                      .remove()
-                      ,
-)
-                               
-.log("Áudio", row.Audio)
-.log("Vídeo", row.Video)
-.log("NOME", getVar("NOME"))
-
 );
 
-newtrial("Final",
-
-newtext("Obrigada pela participação!")
-  .wait()
-
+newTrial("Participante", 
+    newText("<p>Bem-vindo!</p>").center().print()
+    ,
+    newText("<p>Informe o seu nome:</p>").center().print()
+    ,
+    newTextInput("Nome").center()
+    ,
+    newButton("Iniciar").center()
+    ,
+    newVar("NOME")
+      .global()
+      .set(getTextInput("Nome"))
 )
+.log("NOME", getVar("NOME"));
+
+Template("tabela.csv", row =>
+  newTrial("Experimento",
+    
+    newButton("Iniciar vídeo").center().print().wait()
+    ,
+
+    newVideo("Video", row.Video)
+      .size(360,360)
+      .center()
+      .print()
+      .play()
+      .wait()
+    ,
+
+    newText("A", row.julgamentoN),
+    newText("B", row.julgamentoE),
+
+    newCanvas("escolha", "800px", "200px")
+      .add("center at 25%", "middle at 50%", getText("A"))
+      .add("center at 75%", "middle at 50%", getText("B"))
+      .center()
+      .print()
+    ,
+
+    newSelector()
+      .add(getText("A"), getText("B"))
+      .keys("A", "B")
+      .log()
+      .wait()
+    ,
+
+    newButton("Próximo").center().print().wait()
+  )
+  .log("Video", row.Video)
+  .log("NOME", getVar("NOME"))
+);
+
+newTrial("Final",
+  newText("Obrigada pela participação!")
+    .center()
+    .print()
+    .wait()
+);
 
 setOption("countsForProgressBar", false);
